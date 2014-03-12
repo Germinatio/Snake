8c91c52abd702548958570aee9fe2267fa22cfcc
aroblu94 <aroblu94@gmail.com>
2014-03-12 22:29:41 +0100

    Trying to updating the keyListener into game...

--- KL.java
+++ KL.java
@@ -1,29 +1,60 @@
-import java.awt.event.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.*;
 
-public class KL extends KeyAdapter {
-	public void KeyReleased(KeyEvent e) { // when released it keeps the last direction and go on moving 
-		try {
-			Thread.sleep(500);
-		}
-		catch(InterruptedException ex) {}
+public class KL extends JPanel implements KeyListener {
+
+	//CAMPI
+	private JPanel jp;
+	private JLabel l;
+	private JButton b;
+	
+	//COSTRUTTORI
+	public KL() {
+		jp = new JPanel();
+		l = new JLabel();
+		b = new JButton();
+		b.addKeyListener(this);
+		jp.add(b);
+	}
+	
+	//METODI
+	public void keyReleased(KeyEvent e) { // when released it keeps the last direction and go on moving 
 		
 		switch (e.getKeyCode()) 
 		{
 			case KeyEvent.VK_UP:
+				try {
+					Thread.sleep(500);
+				}
+				catch(InterruptedException ex) {}
 				Snake.y--;
 				break;
 			case KeyEvent.VK_DOWN:
+				try {
+					Thread.sleep(500);
+				}
+				catch(InterruptedException ex) {}
 				Snake.y++;
 				break;
 			case KeyEvent.VK_LEFT:
+				try {
+					Thread.sleep(500);
+				}
+				catch(InterruptedException ex) {}
 				Snake.x--;
 				break;
 			case KeyEvent.VK_RIGHT:
+				try {
+					Thread.sleep(500);
+				}
+				catch(InterruptedException ex) {}
 				Snake.x++;
 				break;		
 		}
 	}
-	public void KeyPressed(KeyEvent e) { //when pressed changes direction, then moves
+	
+	public void keyPressed(KeyEvent e) { //when pressed changes direction, then moves
 		switch (e.getKeyCode()) 
 		{
 			case KeyEvent.VK_UP:
@@ -40,4 +71,6 @@ public class KL extends KeyAdapter {
 				break;		
 		}
 	}
+	
+	public void keyTyped(KeyEvent e) {}
 }
--- Test.java
+++ Test.java
@@ -1,20 +1,28 @@
+import javax.swing.*;
+
 public class Test {
 	public static void main(String[] args) {
+		boolean play = true;
 		Table t = new Table();
 		Ent s = new Snake(15, 10);
-		s.setXY();
-		t.add(s, s.x, s.y);
-		while(true) {
+		KL kl = new KL();
+		JFrame jf = new JFrame();
+		jf.getContentPane().add(kl);
+		jf.getContentPane().setBounds(0,0,0,0);
+		jf.setVisible(true);
+		while(play) {
+			s.setXY();
+			t.add(s, s.x, s.y);
 			Ent f = new Food();
 			f.setXY();
 			t.add(f, f.x, f.y);
 			System.out.println(t);
+			t.remove(f, f.x, f.y);
+			System.out.printf("%c[%d;%df", 0x1B, 0, 0);
 			try {
 				Thread.sleep(5000);	//trying "moving" food every 5 sec
 			}
 			catch(InterruptedException ex) {}
-			t.remove(f, f.x, f.y);
-			System.out.printf("%c[%d;%df", 0x1B, 0, 0);
 		}
 	}
 }
