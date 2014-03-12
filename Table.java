b5df9166a43d1dd4195f72ee10ecbd020306d57d
aroblu94 <aroblu94@gmail.com>
2014-03-12 11:29:30 +0100

    Table class now have only a toString method

--- Table.java
+++ Table.java
@@ -1,13 +1,37 @@
+import java.util.*;
+
 public class Table {
+
+	/*
+	 * In order to have a "squared" space 
+	 * in the whole matrix, every space is
+	 * made by 3 spaces
+	 */
 	
 	//CAMPI
-	
+	private String[][] t;
 	
 	//COSTRUTTORE
 	public Table() {
-		
+		t = new String[30][20];
+		for(int x = 0; x < 30; x++) {
+			for(int y = 0; y < 20; y++)
+				t[x][y] = "   ";
+		}
+	}
 
 	//METODI
-	
-	
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("============================================================================================\n");
+		for(int y = 0; y < 20; y++) {
+			sb.append("|");
+			for(int x = 0; x < 30; x++) {
+				sb.append(t[x][y]);
+			}
+			sb.append("|\n");
+		}
+		sb.append("============================================================================================\n");
+		return sb.toString();
+	}
 }
---
+++ Test.java
@@ -0,0 +1,6 @@
+public class Test {
+	public static void main(String[] args) {
+		Table t = new Table();
+		System.out.println(t);
+	}
+}
