bcc9eb2d673385de6cb7f1a845500c3571ba6ab9
nicokant94 <niccolocantu94@hotmail.it>
2014-03-12 12:12:59 +0100

    KeyListener added

---
+++ KL.java
@@ -0,0 +1,38 @@
+import java.awt.event.*;
+
+public class KL extends KeyAdapter{
+	public void KeyReleased(KeyEvent e){ // when released it keeps the last direction and go on moving 
+		switch (e.getKeyCode()) 
+		{
+			case KeyEvent.VK_UP:
+			
+				break;
+			case KeyEvent.VK_DOWN:
+				
+				break;
+			case KeyEvent.VK_LEFT:
+				
+				break;
+			case KeyEvent.VK_RIGHT:
+		
+				break;		
+		}
+	}
+	public void KeyPressed(KeyEvent e){ //when pressed changes direction, then moves
+		switch (e.getKeyCode()) 
+		{
+			case KeyEvent.VK_UP:
+			
+				break;
+			case KeyEvent.VK_DOWN:
+				
+				break;
+			case KeyEvent.VK_LEFT:
+				
+				break;
+			case KeyEvent.VK_RIGHT:
+				
+				break;		
+		}
+	}
+}
