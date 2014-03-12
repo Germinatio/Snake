87a092721a284bd3a11970740e01434feb12b373
nicokant94 <niccolocantu94@hotmail.it>
2014-03-12 11:07:07 +0100

    Ent Fist Mod
    Creata la classe Ent, con le funzioni di base

---
+++ Ent.java
@@ -0,0 +1,17 @@
+public class Ent{
+	/* This class describes the elements that are on the playing table
+	 * Its subclasses are Food and Snake
+	 */
+	//CAMPI
+	String Ent;
+	
+	//COSTRUTTORE
+	public Ent(String s){
+		Ent=s;
+	}
+	
+	//METODI
+	public String toString(){
+		return Ent;
+	}
+}
